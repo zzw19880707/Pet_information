97d4cdbd5ca80b98462eb2f3c02e77e42e6a0b69
zzw19880707 <zzw414851474@qq.com>
2013-12-11 16:24:47 +0800

    更新宏 增加描述

--- PetInfo/PetInfo/kPetInfo.h
+++ PetInfo/PetInfo/kPetInfo.h
@@ -9,12 +9,25 @@
 #ifndef PetInfo_kPetInfo_h
 #define PetInfo_kPetInfo_h
 
+#pragma mark NSNotificationCenter
+//首页加载时,定位完成的通知
+#define isEndLocation @"isEndLocation"
+
+
 #pragma mark UserDefaults
+//判断是否是第一次登陆
 #define isFirstLogin @"isFirstLogin"
+//百度推送绑定信息
 #define BPushchannelid @"BPushchannelid"
 #define BPushappid @"BPushappid"
 #define BPushuserid @"BPushuserid"
-
+//经纬度
+#define user_longitude @"longitude"
+#define user_latitude @"latitude"
+//判断进入程序后是否定位
+#define isLocation @"isLocation"
+//用户登陆后返回的id
+#define user_id @"user_id"
 
 #pragma mark Pet_Color
 #define PetTextColor COLOR(88, 195, 241)
