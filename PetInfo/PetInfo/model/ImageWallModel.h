fbe913667c2148072e24e0e6b3128771b313ba8e
zzw19880707 <zzw414851474@qq.com>
2013-12-11 16:27:32 +0800

    去除图片高宽字段，增加相册预留id

--- PetInfo/PetInfo/model/ImageWallModel.h
+++ PetInfo/PetInfo/model/ImageWallModel.h
@@ -9,8 +9,7 @@
 #import "BaseModel.h"
 
 @interface ImageWallModel : BaseModel
-@property (nonatomic,retain) NSNumber *  width;//图片宽
-@property (nonatomic,retain) NSNumber * height;//图片高
+@property (nonatomic,retain) NSNumber *albumId;//相册id
 @property (nonatomic,retain) NSNumber *imageId;//图片id
 @property (nonatomic,retain) NSNumber *userId;//用户id
 @property (nonatomic,copy) NSString *path;//大图路径
@@ -23,8 +22,8 @@
 @property (nonatomic,retain) NSNumber *pageView;//浏览数
 @property (nonatomic,retain) NSNumber *weekChampion;//周冠军标记
 @property (nonatomic,retain) NSNumber *monthChampion;//月冠军标记
-@property float longitude;//经度
-@property float latitude;//纬度
+@property (nonatomic,retain) NSNumber *longitude;//经度
+@property (nonatomic,retain) NSNumber *latitude;//纬度
 @property (nonatomic,retain) NSNumber * goodFlag;//点赞标识
 @property (nonatomic,retain) NSNumber * badFlag;//点鄙视标识
 
