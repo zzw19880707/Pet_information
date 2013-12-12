d058789dbea65a23893ab6b0b776c4dfa0dc6f44
zzw19880707 <zzw414851474@qq.com>
2013-12-12 14:23:01 +0800

    新增本地文字描述

---
+++ PetInfo/PetInfo/StringValue.h
@@ -0,0 +1,15 @@
+//
+//  StringValue.h
+//  PetInfo
+//
+//  Created by 佐筱猪 on 13-12-12.
+//  Copyright (c) 2013年 佐筱猪. All rights reserved.
+//
+
+#ifndef PetInfo_StringValue_h
+#define PetInfo_StringValue_h
+
+#pragma mark 本地文字描述
+#define INFO_NetNoReachable @"当前网络不可用,请检查网络连接!"
+
+#endif
