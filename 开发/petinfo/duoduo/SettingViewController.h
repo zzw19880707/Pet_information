b95b10d794907906a7fbe0d686125eeff58c22e1
zzw19880707 <zzw414851474@qq.com>
2014-07-21 09:31:44 +0800

    添加视图

---
+++ 开发/petinfo/duoduo/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  宠信
+//
+//  Created by tenyea on 14-7-14.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import "TenyeaBaseViewController.h"
+
+@interface SettingViewController : TenyeaBaseViewController
+
+@end
---
+++ 开发/petinfo/duoduo/SettingViewController.m
@@ -0,0 +1,24 @@
+//
+//  SettingViewController.m
+//  宠信
+//
+//  Created by tenyea on 14-7-14.
+//  Copyright (c) 2014年 zzw. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+@interface SettingViewController ()
+
+@end
+
+@implementation SettingViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+@end
